12321a4fc5e0342a2f9bb2b2a85be3cd3635403a
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-09-12 20:23:33 +0300

    Update diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -90,7 +90,5 @@ rm -f uboot-mtk-20220606/configs/mt7986_netcore_n60_defconfig.orig
 rm -f uboot-mtk-20220606/configs/mt7986_redmi_ax6000_defconfig.orig
 rm -f uboot-mtk-20220606/configs/mt7986_tplink_tl-xdr608x_defconfig.orig
 rm -f uboot-mtk-20220606/configs/mt7986_tplink_tl-xtr8488_defconfig.orig
-rm -f atf-20220606-637ba581b/plat/mediatek/mt7981/drivers/pll/pll.c.orig
-rm -f atf-20231013-0ea67d76a/plat/mediatek/mt7981/drivers/pll/pll.c.orig
 rm -f uboot-mtk-20230718-09eda825/cmd/glbtn.c.orig
 rm -f uboot-mtk-20220606/cmd/glbtn.c.orig
